ce24bd1684603e415a32e75000b8beb8d459a371
prashant369-designer <prashant602102@gmail.com>
2025-09-30 21:18:58 +0530

    tricky change

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use("/api/orders", require("./routes/order.routes"));
 
 
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get("*", (req, res) =>
+app.get("*:", (req, res) =>
   res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
 );
 
